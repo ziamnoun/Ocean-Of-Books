cf8dcece9debf3053c5c03a9fbfc40317aeb81d6
ziamnoun <ziamziam05@gmail.com>
2024-03-26 04:32:00 +0600

    another

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Navbar from './assets/Components/Navbar.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<Navbar></Navbar>
   },
 ]);
 
