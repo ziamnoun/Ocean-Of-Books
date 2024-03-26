b44ff28f7f07896f31e904e2c20cb7e6a5a6d897
ziamnoun <ziamziam05@gmail.com>
2024-03-26 22:27:52 +0600

    home page done

--- src/assets/Components/BookBuffet.jsx
+++ src/assets/Components/BookBuffet.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
 
-const BookBuffet = () => {
+const BookBuffet = ({books}) => {
+
+    console.log(books);
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl mt-10 border-2 border-gray-300 ">
-  <div className="bg-gray-100 m-5 rounded-lg"><figure><img src="public/Images/pngwing 1.png" alt="Books" /></figure></div>
+  <div className="bg-gray-100 m-5 rounded-lg"><figure><img src={books.image} alt="Books" /></figure></div>
   <div className="card-body">
     <div className="categoryBooks flex gap-2">
-        <p className='border-2 border-green-50 bg-green-50 rounded-xl text-green-500 text-center font-bold text-xl'>Young</p>
-        <p className='border-2 border-green-50 bg-green-50 rounded-xl text-green-500 text-center font-bold text-x'>Adult</p>
+        <p className='border-2 border-green-50 bg-green-50 rounded-xl text-green-500 text-center font-bold text-xl'>{books.tags[0]}</p>
+        <p className='border-2 border-green-50 bg-green-50 rounded-xl text-green-500 text-center font-bold text-x'>{books.tags[1]}</p>
     </div>
     <h2 className="card-title text-3xl font-bold">
-    The Catcher in the Rye
-
-      
+    {books.bookName}
     </h2>
-    <p className='font-semibold text-lg border-b-2 border-b-stone-200 border-dashed'>By : Awlad Hossain</p>
+    <p className='font-semibold text-lg border-b-2 border-b-stone-200 border-dashed'>{books.author}</p>
     
     <div className="card-actions justify-between">
-        <p>Fiction</p>
+        <p>{books.category}</p>
       <div>
-        <p>5.0  </p>
+        <p>{books.rating} </p>
       </div>
     </div>
   </div>
--- src/assets/Components/Home.jsx
+++ src/assets/Components/Home.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import Banner from './Banner';
 import BookBuffet from './BookBuffet';
 
 
 const Home = () => {
+    const[booklist,setBooklist]=useState([])
+
+
+
+    useEffect(()=>{
+        fetch("public/fakeDataOfBooks.json")
+        .then(res=>res.json())
+        .then(data=>{
+            setBooklist(data);
+        })
+    },[]);
+console.log(booklist)
+
+
     return (
         <div>
       <Banner></Banner>
      <div>
         <p className='text-5xl font-extrabold text-center mt-10'>Books</p>
      </div>
-        <BookBuffet></BookBuffet>
+        
+        <div className='lg:w-[80%] mx-auto grid lg:grid-cols-3 grid-cols-1 '>
+            {
+                booklist.map(item=><BookBuffet  books={item}></BookBuffet>)
+            }
+
+
+
+        </div>
       </div>
        
     );
